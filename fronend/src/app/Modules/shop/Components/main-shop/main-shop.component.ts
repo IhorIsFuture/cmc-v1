a71109bad8b3257fa163ecdba1c359b26701597b
zharyiihor <zhariy.ihor@fornova.net>
2019-03-10 13:10:09 +0200

    style

---
+++ fronend/src/app/Modules/shop/Components/main-shop/main-shop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-shop',
+  templateUrl: './main-shop.component.html',
+  styleUrls: ['./main-shop.component.less']
+})
+export class MainShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
